e0c66f47626bdfba8fcf3892ad401fec41a064bf
Eric Nelson <nelseric@mail.gvsu.edu>
2012-11-13 22:24:06 -0500

    Initial

---
+++ mmu_sim.rb
@@ -0,0 +1,35 @@
+class MMU
+
+  def initialize
+
+  end
+
+  def op(args)
+    p args
+  end
+
+
+end
+
+
+if __FILE__ == $0
+  if ARGV[0] and File.exist? ARGV[0]
+    tapef = File.open ARGV[0], "r"
+    tape = tapef.read
+    lreg = /(\d+) (([a-zA-Z_ ]+)|(\d+) (\d+))/
+
+    mm = MMU.new
+
+    tape.split("\n").each do |line|
+      md = line.match(lreg)
+      if md[3] == nil
+        mm.op :pid => md[1].to_i, :op => :alloc, :text => md[4].to_i, :data => md[4].to_i
+
+      elsif md[3].match /[hH]alt/
+        mm.op :pid => md[1].to_i, :op => :halt
+      end
+    end
+  else
+    print "Usage: #{$0} (tape file name)"
+  end
+end
